c4817b530de167d1e6ac66e37d766d5782a2d644
Kyle Jones <mynameiskyle242@gmail.com>
2021-06-19 13:42:58 -0400

    Initial commit

---
+++ src/main/java/com/joneskyle/bourdain/BourdainApplication.java
@@ -0,0 +1,13 @@
+package com.joneskyle.bourdain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BourdainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BourdainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joneskyle/bourdain/BourdainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joneskyle.bourdain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BourdainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
